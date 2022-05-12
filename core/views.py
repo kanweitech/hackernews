230ab2ff7c1834e39de915dd0305742d2f8772df
kanweitech <kanweiedward>
2022-05-12 12:17:03 +0100

    new commits

--- core/views.py
+++ core/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+import requests
 
-# Create your views here.
+
+def news(request):
+
+	#pull data from third party rest api
+	response = requests.get('https://hackernews.api-docs.io')
+
+	#convert response data into json
+	news = response.json()
+
+	return HttpResponse("news")
+	pass
