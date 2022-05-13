a10c3e72295ae5d2fe5507c1477ca669ba7bcc22
kanweitech <kanweiedward>
2022-05-13 18:49:05 +0100

    new commit

--- news/admin.py
+++ news/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import LatestStory, Comment
 
-# Register your models here.
+@admin.register(LatestStory)
+class LatestStoryAdmin(admin.ModelAdmin):
+	list_display = ["unique_api_story_id", "story_type", "author", "title", "story_url", "parent_id"]
+	prepopulated_fields = {"slug": ("title",)}
+	search_fields = ["title", "story_type", "author", "created_by", "unique_api_story_id"]
+	list_per_page = 20
+	list_filter = ["story_type",]
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+	 list_display = ["unique_comment_api_id", "author", "title", "comment_url"]
+	 list_per_page = 20
+
+
+
